9b0848a434c3521c1260005b400e8891a53751a4
nahsooyeon <nahsooyeon@gmail.com>
2021-09-09 17:39:29 +0900

    🔨 추가: postgres DB 연결 샘플코드 추가

--- pg/insert.js
+++ pg/insert.js
@@ -1,5 +1,13 @@
 const { Client } = require("pg");
 
+const dbconfig = {
+  host: "process.env.DB_HOST",
+  user: "process.env.DB_USER",
+  password: "process.env.",
+  database: "",
+  port: "",
+};
+
 const client = new Client({
   user: "DB 사용자 명",
   host: "DB 주소",
@@ -8,7 +16,18 @@ const client = new Client({
   port: 5432,
 });
 
-client.connect();
+/* const client = new Client(dbconfig) */
+
+client.connect((err) => {
+  if (err) {
+    console.error("Failed to connect db due to" + err);
+  } else {
+    console.log("Connected to DB");
+  }
+});
+
+/* 정상적으로 연결이 완료되면 선언한 pg client 객체를 이용해 db 쿼리를 날린다.
+결과값은 promise의 형태로도 받을 수 있고, 비동기 콜백을 피하기 위해서 await 로 받을 수도 있다. */
 
 const sql =
   "INSERT INTO userList (id, name, nickname, email, password, favorite_type, favorite_country) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *";
